c991512166b654a9285254721891a3808fd2fb36
B6Infinity <broteendas.official@gmail.com>
2022-08-31 11:16:51 +0530

    idk

--- availme/urls.py
+++ availme/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 
 import availme_webapp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('website.urls')),
+    # path('', include('website.urls')),
+    path('', include('availme_webapp.urls')), # FOr now
+
     path('app/', include('availme_webapp.urls')),
-]
+] + staticfiles_urlpatterns()
