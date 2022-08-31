5e462de62a59120f6f5599e4e5726000c633f548
B6Infinity <broteendas.official@gmail.com>
2022-08-31 10:47:35 +0530

    added apps

--- availme/settings.py
+++ availme/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # MANUALLY ADDED
+    'availme_webapp',
+    'website',
 ]
 
 MIDDLEWARE = [
