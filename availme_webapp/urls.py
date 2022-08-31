04411c91ee9f491469c812a55beb1caf90d5b6bc
B6Infinity <broteendas.official@gmail.com>
2022-08-31 19:09:11 +0530

    frontend ui dont

--- availme_webapp/urls.py
+++ availme_webapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', view=views.index, name='app_home'),
+    path('appfetch', view=views.master_api, name='master_api_gateway'),
 ]
--- availme_webapp/views.py
+++ availme_webapp/views.py
@@ -1,5 +1,25 @@
+from time import sleep
 from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def master_api(request):
+    if request.user.username == 'broteen':
+
+        page_id = request.POST['requested_page']
+
+        html = f'''<h1>THIS IS BEGINNIN of {page_id}</h1>
+        '''
+        sleep(2)
+        return JsonResponse({
+            "content": {
+                "html": html,
+                "page": page_id
+            }
+        })
+    else:
+        return JsonResponse({"FORBIDDEN": "Authentication Error", "ELABORATION": "Unauthenticated Request"})
+
